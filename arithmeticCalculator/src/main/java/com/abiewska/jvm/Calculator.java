541c4a1faba2eb2bc69923760be16669a17e8396
abie115 <donicczka@gmail.com>
2016-03-31 15:12:41 +0200

    add calc

---
+++ arithmeticCalculator/src/main/java/com/abiewska/jvm/Calculator.java
@@ -0,0 +1,12 @@
+package com.abiewska.jvm;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Calculator {
+
+	public static BigDecimal roundNumber(double n) {
+		return new BigDecimal(n).setScale(2, RoundingMode.HALF_UP);
+	}
+
+}
